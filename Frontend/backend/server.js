9ce87e246266ef2f4c75838a3bce6f6d26a41444
Ragul A (Digital) <RA351953@wipro.com>
2020-02-25 18:57:43 +0530

    creating package.json for backend

--- Frontend/backend/server.js
+++ Frontend/backend/server.js
@@ -9,7 +9,7 @@
 // const server = http.createServer(app);
 // server.listen(port);
 
-const app = require("./backend/app");
+const app = require("./app");
 const debug = require("debug")("node-angular");
 const http = require("http");
 
